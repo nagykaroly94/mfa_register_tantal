62a3c904a25a48d624fad163838c157696c0cc78
nagykaroly94 <nagykaroly94@icloud.com>
2024-12-06 12:57:12 +0100

    Database
    Az adatbázis táblájának létrehozásához szükséges SQL utasítás-

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `adatok` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nev` varchar(255) NOT NULL,
+  `adoszam` varchar(50) NOT NULL,
+  `felhasznalonev` varchar(255) NOT NULL,
+  `jelszo` varchar(255) NOT NULL,
+  `telefonszam` varchar(20) NOT NULL,
+  `emailcim` varchar(255) DEFAULT NULL,
+  `torlokod` varchar(50) DEFAULT NULL,
+  `onlineszamla_nev` varchar(255) DEFAULT NULL,
+  `onlineszamla_jelszo` varchar(255) DEFAULT NULL,
+  `qr_kod` text,
+  `ugyintezo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
